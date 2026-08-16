2a8b1998e59f858f892e6c7fae07fb0d98517a3d
omkarajure-stack <omkarajure@gmail.com>
2026-08-16 15:29:53 +0530

    first comit

---
+++ 5.c
@@ -0,0 +1,101 @@
+// opretor
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int a = 10, b = 6,c;
+//     printf("1=arthmatic opreter\n");
+//     printf("a / b = %d\n,",a / b);
+//     printf("a * b = %d\n",a * b);
+//     printf("a - b = %d\n",a - b);
+//     printf("a + b = %d\n",a + b);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 10, b = 8,c;
+//     printf("2=relational opretor\n");
+//     printf("a == b %d\n",a == b);
+//     printf("a != b %d\n",a != b);
+//     printf("a < b %d\n",a < b);
+//     printf("a > b %d\n",a > b);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 10, b = 3, c;
+//     printf("3=logical opretor");
+//     printf("(a < b) && (b < 0)%d\n",(a < b) && (b < 0));
+//     printf("(a > b) || (b > 0)%d\n",(a > b) || (b > 0));
+//     printf("!(a == b)%d\n",!(a == b));
+//     return 0;
+// }
+
+//  #include<stdio.h>
+//   int main()
+//   {
+//      int a = 2, b = 3,c;
+//      printf("4=assinment oprear\n");
+//      c = a;
+//      printf("c = a%d\n",c);
+//      c += b;
+//      printf("c += b%d\n",c);
+//      c -= b;
+//      printf("c -= b%d\n",c);
+//      c *= b;
+//      printf("c *= b%d\n",c);
+//      return 0;
+//   }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 10, b = 5,c;
+//     printf("5=increament/decrement\n");
+//     printf("++a = %d\n",++a);
+//     printf("a++ = %d\n",a++);
+//     printf("b-- =%d\n",b--);
+//     printf("--b =%d\n",--b);
+//     return 0;
+// }
+
+
+//  #include<stdio.h>
+//  int main()
+//  {
+//      int a = 10,b = 9,c;
+//       printf("6=conditional opretor\n");
+//       int max = (a = b) ? a : b;
+//       printf("max of a & b%d\n",max);
+//       return 0;
+//  }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 10, b = 5, c;
+//     printf("7=bitwise opretor\n");
+//     printf("a & b =%d\n",a & b);
+//     printf("a | b =%d\n",a | b);
+//     printf("a ^ b =%d\n",a ^ b);
+//     printf("a << 1= %d\n",a << b);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 5,b = 8,c;
+//     printf("8=special opretor\n");
+//     printf("sizeof(int) = %zu\n",sizeof(int));
+//     return 0;
+// }
+
---
+++ 6.c
@@ -0,0 +1,109 @@
+// conditional statement{if/if else/if else if)
+
+
+// #include<stdio.h>
+// int main()
+// {
+        // if
+
+//     if(10 < 5){
+//                              printf(" a grether than 5 ");
+
+//               }
+//              return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+
+        // if else
+
+//     if(10 < 7){
+//                   printf("10 grether than 7");
+//               }
+          
+          
+//     else {
+//                printf("10 less than 7");
+            
+//          }
+//             return 0;
+
+// }
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int x = 5;
+//     int y = 8;
+        // nested if statement
+        
+//     if(x > 3){
+//                    printf("x grethr than 3\n");
+//              }
+//              if(y < 5){
+//                            printf("y less than 5\n");
+//                       }
+//                       return 0;
+// }
+
+
+// #include<stdio.h>
+// #include<stdbool.h>
+// int main ()
+// {
+//        // nested if else statement
+//        int age = 20;
+//        bool iscitizen = true;
+
+//        if(age >= 18){
+//                          printf("old you can vote\n");
+                      
+//       if(iscitizen){   
+//                             printf("you citizen you vote\n");
+
+                        
+//            }     else{
+//                              printf("you can not citzen you cannot vote\n");
+                             
+//                         }
+
+//        }
+//                             else{
+//                                       printf("not old you cannot vote\n");
+//                                 }
+//                                 return 0;
+// }
+
+
+
+// #include<stdio.h>
+// #include<stdbool.h>
+// int main()
+// {
+//     int age = 20;
+//     bool citizen = true;
+//     if( age >=18){
+//         printf("old you can vote\n");
+//         if(citizen){
+//                       ("you can citizen you vote\n");
+
+
+//                     }   
+//                       else{
+//                             printf("you not citizen you cannot vote\n");
+
+//                           }
+//                         }          
+//                    else{
+//                            printf("you not old you cannot vote\n");
+
+//                        }
+                     
+//                                                 return 0; 
+
+
+// }
