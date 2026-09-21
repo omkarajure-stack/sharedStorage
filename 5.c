f13c1c3b87f9613b8640699b7275c5831a5b563f
omkarajure-stack <omkarajure@gmail.com>
2026-09-21 13:21:18 +0530

    add my code

---
+++ 5.c
@@ -0,0 +1,85 @@
+//  #include<stdio.h>
+//  int main()
+//  {
+
+//   int i;
+//   int marks[4] = {1,2,3,4};
+//   printf("one D array\n");
+//   for(i=0;i<4;i++)
+//   {
+//      printf("%d     ",marks[i]);
+//   }
+
+//   return 0;
+
+
+// }
+
+
+
+//  #include<stdio.h>
+//  int main()
+//  {
+
+//  int a[3][2]={
+//                   {1,2,},
+//                   {4,5,},
+//                   {7,8,}
+//  };
+
+//  int i,j;
+
+//  printf("printing elments\n");
+//  for(i=0;i<3;i++)
+//  {
+//      for(j=0;j<2;j++)
+//      {
+//          printf("%d\t",a[i][j]);
+//      }
+//      printf("\n");
+//  }
+//  return 0;
+
+
+//  }
+
+
+
+
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a[3][3],i,j;
+//     for(i=0;i<3;i++)
+//     {
+//         for(j=0;j<3;j++)
+//         {
+//             printf("[%d][%d]",i,j);
+//             scanf("%d",&a[i][j]);
+//         }
+//     }
+
+
+//     printf("printing elements\n");
+//     for(i=0;i<3;i++)
+//     {
+//         printf("\n");
+//         for(j=0;j<3;j++)
+//         {
+//             printf("%d\t",a[i][j]);
+//         }
+//     }
+//        return 0;
+    
+
+
+// }
+
+
+
+
+
+
